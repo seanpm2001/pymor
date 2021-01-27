01415954725f079f40d061c1e5b2257007ce1863
René Fritze <rene.fritze@wwu.de>
2021-01-27 14:31:55 +0100

    [docs] resort imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,15 +4,18 @@
 
 import sys, os, re
 import slugify
+import glob
+import sphinx
+from pathlib import Path
 
-os.environ['PYMOR_WITH_SPHINX'] = '1'
+import substitutions
 
 # Check Sphinx version
-import sphinx
 if sphinx.__version__ < "1.7":
     raise RuntimeError("Sphinx 1.7 or newer required")
 
 needs_sphinx = '1.7'
+os.environ['PYMOR_WITH_SPHINX'] = '1'
 
 # -----------------------------------------------------------------------------
 # General configuration
@@ -221,7 +224,6 @@ latex_use_modindex = False
 # Autosummary
 # -----------------------------------------------------------------------------
 
-import glob
 autosummary_generate = glob.glob("generated/*.rst")
 
 # -----------------------------------------------------------------------------
@@ -250,7 +252,6 @@ intersphinx_mapping = {'python': ('https://docs.python.org/3', None),
                        'matplotlib': ('https://matplotlib.org', None),
                        'Sphinx': ('https://www.sphinx-doc.org/en/stable/', None)}
 
-import substitutions
 rst_epilog = substitutions.substitutions
 
 modindex_common_prefix = ['pymor.']
